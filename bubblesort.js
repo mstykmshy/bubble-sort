3633bd22e7f9bec359a0fbbb0043d484efdbd8ba
Saj King <a.sajking@gmail.com>
2020-04-07 14:44:37 -0400

    bubble merge solved

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,14 @@
-const bubbleSort = () => {
-  return 5;
+function bubbleSort (arr) {
+  let origArgs = Array.from(arr)
+  let swaps = 0
+  for (let i = 0 ; i < arr.length -1 ; i++){
+    if (arr[i]>arr[i+1]){
+      let x = arr[i]
+      arr[i]=arr[i+1];
+      arr[i+1] = x;
+      swaps ++
+    }
+  }
+  if (swaps>0) bubbleSort(arr)
+  return arr
 };
